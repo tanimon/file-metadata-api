c547f9df15ff063796681c29a86d208a7e6567b3
tanimon <8575113+tanimon@users.noreply.github.com>
2021-08-19 19:49:52 +0900

    Reformat `server.js`

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
 var app = express();
 
@@ -8,13 +8,10 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
